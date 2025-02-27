da65296e4cff2ae00c12445ea085ddddebf39de1
Umendra A Bisen <umendrabisen2005@gmail.com>
2025-02-27 18:22:57 +0530

    Update script.js

--- Script/script.js
+++ Script/script.js
@@ -59,7 +59,7 @@ document.getElementById("contact-form").addEventListener("submit", function(even
   var email = document.getElementById("email").value;
   var subject = document.getElementById("subject").value;
   var message = document.getElementById("message").value;
-  var recipientEmail = "rohansamuel134@gmail.com";
+  var recipientEmail = "umendrabisen40@gmail.com";
 
   if (name && email && message) {
       var mailtoLink = `mailto:${recipientEmail}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(name)} (${encodeURIComponent(email)}): ${encodeURIComponent(message)}`;
